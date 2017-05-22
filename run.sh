3d6d0ea0d6f921a01e9eea9caa04b446fbb3dfef
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2017-05-22 14:43:24 +0800

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+./build/test_face_verification.bin   \
+    ./1.jpg \
+    ./2.jpg \
+    ./SeetaFaceEngine/FaceIdentification/model/seeta_fd_frontal_v1.0.bin  \
+    ./SeetaFaceEngine/FaceIdentification/model/seeta_fa_v1.1.bin \
+    ./SeetaFaceEngine/FaceIdentification/model/seeta_fr_v1.0.bin
+
+
